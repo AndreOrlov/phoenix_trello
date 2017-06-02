d50789eb709e64436be34fd3a6cf5cc84ed3742e
AndreOrlov <vkstan@ya.ru>
2017-06-02 12:55:52 +0300

    Init commit. Phoenix started wo error

---
+++ web/router.ex
@@ -0,0 +1,19 @@
+# master/web/router.ex
+
+defmodule PhoenixTrello.Router do
+  use PhoenixTrello.Web, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  scope "/", PhoenixTrello do
+    pipe_through :browser # Use the default browser stack
+
+    get "*path", PageController, :index
+  end
+end
